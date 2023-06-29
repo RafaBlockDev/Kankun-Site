d458a635b0dc918deaf61235beb232315ff6b63f
RafaCypherpunk <rafafuentes@Rafas-Mac-mini.local>
2023-06-28 22:14:45 -0600

    fix: change text

--- components/hero.js
+++ components/hero.js
@@ -21,7 +21,7 @@ const Hero = () => {
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
-                More 📚
+                Holding 📚
               </a>
 
             </div>
